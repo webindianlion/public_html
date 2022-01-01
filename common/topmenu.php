6666766c47a4135c517c7dc2768e0a5ce3b6a7d3
Vinod Kumar <webindianlion@gmail.com>
2022-01-02 01:22:02 +0530

    hiding menu

--- common/topmenu.php
+++ common/topmenu.php
@@ -2,17 +2,17 @@
 <nav class="navbar navbar-expand-lg navbar-dark  topMenu nav" aria-label="Fourth navbar example">
     <div class="container">
       <h2 class="logo"> <a href="index.php"> inWebService </a> </h2>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+      <!-- <button class="navbar-toggler d-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
-      <div class="collapse navbar-collapse" id="navbarsExample04">
+      <div class="collapse navbar-collapse d-none" id="navbarsExample04">
         <ul id="" class="navbar-nav ms-auto mb-md-0">
             <li class="nav-item"> <a class="nav-link active" aria-current="page" href="about.php"> About Us </a> </li> 
             <li class="nav-item"> <a class="nav-link active" aria-current="page" href="development.php"> Web Development </a> </li>            
             <li class="nav-item"> <a class="nav-link active" aria-current="page" href="marketing.php"> Digital Marketing </a> </li> 
             <li class="contactUs"> <a class="nav-link active" aria-current="page" href="contact.php"> Contact Us </a> </li>          
         </ul>
-      </div>
+      </div> -->
     </div>
   </nav>
