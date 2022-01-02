f04264c7d15fa5cd6d8ea5e260bd075882a50cb7
Vinod Kumar <webindianlion@gmail.com>
2022-01-02 16:41:29 +0530

    home page content change

--- index.php
+++ index.php
@@ -7,18 +7,18 @@
     <?php include './common/topmenu.php' ?>
 
     <div class="containerr videoContainer">
-        <div id="particles-js"> 
+        <div id="particles-js">
             <canvas class="particles-js-canvas-el" width="659" height="472"></canvas>
         </div>
         <div class="videoText d-flex flex-column justify-content-center align-items-center position-absolute h-100 container-fluid">
-            <h1 class="">For impactful and innovative digital solutions</h1>
+            <h1 class="">For Impactful And Innovative Digital Solutions!</h1>
             <p class="pt-2">You can choose us as your technology partner.</p>
         </div>
     </div>
 
     <section class="container whyWebsite py-5">
-        <h3 class="ttl text-primary">Why your business need a website?</h3>
-        <p class="subTtl">Lets give more space to your business to expend via internet with the website wings.</p>
+        <h3 class="ttl text-primary">Why does your business need a website?</h3>
+        <p class="subTtl">Let us give more space to your business to expand via the internet with the website wings.</p>
         <div class="row pt-3">
             <div class="col-12">
                 <img src="./images/why-your-business-need-website.jpg" class="w-100 shadow-sm p-2 rounded">
@@ -26,16 +26,16 @@
             <div class="col-12 textPart">
                 <div class="list-group p-2">
                     <a href="#" class="list-group-item list-group-item-action list-group-item-primary">Your business brand credibility is proven by your business website.</a>
-                    <a href="#" class="list-group-item list-group-item-action list-group-item-warning"> Your business brand are Easily Accessible with business website. </a>
-                    <a href="#" class="list-group-item list-group-item-action list-group-item-success"> Your business website builds a relationship with your existing clients and your potietial. </a>
-                    <a href="#" class="list-group-item list-group-item-action list-group-item-danger"> Your business website can be a Showcase Your Products or Services. </a>
-                    <a href="#" class="list-group-item list-group-item-action list-group-item-warning"> Your business website increase the visibility of your brand. </a>
+                    <a href="#" class="list-group-item list-group-item-action list-group-item-warning"> Your business brand is Easily Accessible with a business website. </a>
+                    <a href="#" class="list-group-item list-group-item-action list-group-item-success"> Your business website builds a relationship with your existing and your potential clients. </a>
+                    <a href="#" class="list-group-item list-group-item-action list-group-item-danger"> Your business website can be a Showcase of Your Products or Services. </a>
+                    <a href="#" class="list-group-item list-group-item-action list-group-item-warning"> Your business website increases the visibility of your brand. </a>
                     <a href="#" class="list-group-item list-group-item-action list-group-item-info"> Your business website always gives you the benefit of Google Searches </a>
                     <a href="#" class="list-group-item list-group-item-action list-group-item-dark"> Your business website can strategically target your customers. </a>
-                    <a href="#" class="list-group-item list-group-item-action list-group-item-warning"> Your business website always an important tool of online marketing. </a>
-                    <a href="#" class="list-group-item list-group-item-action list-group-item-primary"> Most impotantly All of your competitors are already having a business website. </a>
+                    <a href="#" class="list-group-item list-group-item-action list-group-item-warning"> Your business website is always an important tool of online marketing. </a>
+                    <a href="#" class="list-group-item list-group-item-action list-group-item-primary"> Most importantly All of your competitors are already having a business website. </a>
                 </div>
-            </div>            
+            </div>
         </div>
     </section>
 
@@ -102,13 +102,7 @@
                 <p> PHP </p>
             </div>
             <div class="cardd col" data-aos="fade-up">
-                <p> Laravel </p>
-            </div>
-            <div class="cardd col" data-aos="fade-up">
                 <p> Node </p>
-            </div>
-            <div class="cardd col" data-aos="fade-up">
-                <p> VUE </p>
             </div>
             <div class="cardd col" data-aos="fade-up">
                 <p> Mongo DB </p>
@@ -119,34 +113,26 @@
         </div>
     </section>
     <div class="b-example-divider"></div>
-    <section class="container py-5">
+    <!-- <section class="container py-5">
         <h3 class="ttl text-primary"> Who we are? </h3>
-        <!-- <p class="subTtl"> Sub-Title </p> -->
+        <p class="subTtl"> Sub-Title </p>
         <div class="row">
             <p class="lead text-center"> inWebService is a web development Solution provider. Established in 2013 serving domestic and offshore clients in various technical aspects. Our vision is to give technical support to our client's business. To achieve our vision, our team strives to deliver innovative, reliable and cost effective solutions and services. We have the ability to discover, design, develop and deliver both inative and Cross-platform mobile applications for all the platforms in trend. inWebService deals with all kinds of website services. An ideal workflow process is being followed by us. We get encouragement from our clients for our fair and innovative work. Also, we keep updating the methods and techniques of our working. inWebService are here to serve the best web development services to our clients.
             </p>
         </div>
-    </section>
-    <div class="b-example-divider"></div>
+    </section> -->
+    <!-- <div class="b-example-divider"></div> -->
     <section class="container py-5">
         <h3 class="ttl text-primary"> Why Choose Us </h3>
         <!-- <p class="subTtl"> Sub-Title </p> -->
         <div class="row">
-            <p class="lead text-center"> inWebService is a ideal website development service provider. inWebService has newly adopted a customer focused approach and an easy way to communicate with them. We deliver strong and recently updated content to our clients. Here, we design and develop the applications as per the need of our customers. A team of highly skilled workers is available 24 hours for any kind of updation. We deliver quality services at considerable prices. </p>
-            <div class="row py-3">
-                <div class="col-md-6 mb-3">
-                    <div class="card">
-                        <div class="card-body">
-                            <h4 class="text-primary"> Reliable </h4>
-                            <p class="fw-normal"> You can trust inWebService in terms of quality and cost of services. Our company is the most reliable source of information. We work efficiently and in an integrated environment. We completely perform under the conditions given by our clients. </p>
-                        </div>
-                    </div>
-                </div>
+            <p class="lead text-center"> inWebService is a ideal website development service in industry. Follwoing is the resson to choose us first. </p>
+            <div class="row py-3">                
                 <div class="col-md-6 mb-3">
                     <div class="card">
                         <div class="card-body">
                             <h4 class="text-primary"> Support & Service </h4>
-                            <p class="fw-normal"> Our company is available all working days for our customers. We provide complete support to our customers. All types of website testing, and maintenance is provided by us. </p>
+                            <p class="fw-normal"> inWebService support our customers whenever they need us. </p>
                         </div>
                     </div>
                 </div>
@@ -154,15 +140,23 @@
                     <div class="card">
                         <div class="card-body">
                             <h4 class="text-primary"> Experienced </h4>
-                            <p class="fw-normal"> inWebService have 7 years of experience in the field of websites development. We successfully managed to deal with thousands of clients in past years. We work and take decisions on the basis of our past experience. </p>
+                            <p class="fw-normal"> inWebService have many years of experience to move their customers upto their goels.</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-6 mb-3">
                     <div class="card">
                         <div class="card-body">
-                             <h4 class="text-primary"> Quality Assurance </h4>
-                            <p class="fw-normal"> inWebService ensures you to provide standard quality of services at a reasonable price. We aim to satisfy the requirements and demands of our customers.Various innovative working methods are adopted by us to fullfill needs. </p>
+                            <h4 class="text-primary"> Quality Assurance </h4>
+                            <p class="fw-normal"> inWebService ensures you to provide standard quality of services.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 mb-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="text-primary"> Reliable </h4>
+                            <p class="fw-normal"> We will get our fruits after you. </p>
                         </div>
                     </div>
                 </div>
@@ -172,54 +166,54 @@
     </section>
     <div class="b-example-divider"></div>
     <section class="container my-5">
-        <h3 class="ttl text-primary"> Strategy We use to Buid Website </h3>
-        <p class="subTtl"> To create a website, we have to follow the various strategies. First of all, we have to choose our goal. Then we check the scope to achieve our goal. A perfect strategy is chosen and a site is created. </p>
+        <h3 class="ttl text-primary"> Strategy we use to be done. </h3>
+        <p class="subTtl"> To give you a latest and full functioning tool we use the follwing stategies. </p>
         <div class="row py-3">
             <div class="col-md-6 mb-3">
                 <div class="card">
                     <div class="card-body">
-                         <h4 class="text-primary"> Requirment </h4>
-                        <p> The complete information regarding content of the website is searched and useful data is edited on the webpage. Taste and preference of the customer should be well known to the web developer. </p>
+                        <h4 class="text-primary"> Requirment </h4>
+                        <p> We will get your requirment first. </p>
                     </div>
                 </div>
             </div>
             <div class="col-md-6 mb-3">
                 <div class="card">
                     <div class="card-body">
-                         <h4 class="text-primary"> Research </h4>
-                        <p> The complete information regarding content of the website is searched and useful data is edited on the webpage. Taste and preference of the customer should be well known to the web developer. </p>
+                        <h4 class="text-primary"> Research </h4>
+                        <p> We will give full research to your requirment to find out a best solution. </p>
                     </div>
                 </div>
             </div>
             <div class="col-md-6 mb-3">
                 <div class="card">
                     <div class="card-body">
-                         <h4 class="text-primary"> Design </h4>
-                        <p> Designing a website refers to the process of creating web pages. Our company edits different varieties of images, animations, graphics, texts, contents, links, compilers and codes in each website. </p>
+                        <h4 class="text-primary"> Design </h4>
+                        <p> We will desing your solution to best user experience. </p>
                     </div>
                 </div>
             </div>
             <div class="col-md-6 mb-3">
                 <div class="card">
                     <div class="card-body">
-                         <h4 class="text-primary"> Development </h4>
-                        <p> An attractive subs eye catching website is created by inWebService to draw the attention of clients towards the company. It leads to improved user's experience and makes an increase in mobile traffic. </p>
+                        <h4 class="text-primary"> Development </h4>
+                        <p> We will develop your design with latest technology </p>
                     </div>
                 </div>
             </div>
             <div class="col-md-6 mb-3">
                 <div class="card">
                     <div class="card-body">
-                         <h4 class="text-primary"> Testing </h4>
-                        <p> An attractive subs eye catching website is created by inWebService to draw the attention of clients towards the company. It leads to improved user's experience and makes an increase in mobile traffic. </p>
+                        <h4 class="text-primary"> Testing </h4>
+                        <p> We will test your website before to go live. </p>
                     </div>
                 </div>
             </div>
             <div class="col-md-6 mb-3">
                 <div class="card">
                     <div class="card-body">
-                         <h4 class="text-primary"> Deployment </h4>
-                        <p> An attractive subs eye catching website is created by inWebService to draw the attention of clients towards the company. It leads to improved user's experience and makes an increase in mobile traffic. </p>
+                        <h4 class="text-primary"> Deployment </h4>
+                        <p> At last we will live your website to your customers. </p>
                     </div>
                 </div>
             </div>
@@ -235,20 +229,38 @@
     </section>
     <div class="b-example-divider"></div>
     <section class="container py-5">
-        <h3 class="ttl text-primary"> 6999 INR Only </h3>
+        <h3 class="ttl text-primary"> Features to add your website </h3>
         <p class="subTtl"> Simply Contact Us To Know More </p>
         <div class="row pt-3">
             <div class="cardd col" data-aos="fade-up">
-                <p> AMP Enabled </p>
+                <p> Desktop/ Mobile Responsive </p>
             </div>
             <div class="cardd col" data-aos="fade-up">
-                <p> 1 Yr Hosting </p>
+                <p> Official Emails </p>
             </div>
             <div class="cardd col" data-aos="fade-up">
-                <p> 1 Yr SSL Security </p>
+                <p> Logo + Favicon </p>
             </div>
             <div class="cardd col" data-aos="fade-up">
-                <p> Payment Gateway (Ecommerce) </p>
+                <p> Content & Graphics </p>
+            </div>
+            <div class="cardd col" data-aos="fade-up">
+                <p> Long Term Support </p>
+            </div>
+            <div class="cardd col" data-aos="fade-up">
+                <p> Seo </p>
+            </div>
+            <div class="cardd col" data-aos="fade-up">
+                <p> Products Catalogue </p>
+            </div>
+            <div class="cardd col" data-aos="fade-up">
+                <p> Contact Form </p>
+            </div>
+            <div class="cardd col" data-aos="fade-up">
+                <p> Social Links </p>
+            </div>
+            <div class="cardd col" data-aos="fade-up">
+                <p> Google Business Listing </p>
             </div>
             <div class="cardd col" data-aos="fade-up">
                 <p> Chat Bot </p>
@@ -260,55 +272,16 @@
                 <p> Dynamic Layout </p>
             </div>
             <div class="cardd col" data-aos="fade-up">
-                <p> Desktop/ Mobile Responsive </p>
-            </div>
-            <div class="cardd col" data-aos="fade-up">
-                <p> On Page Seo </p>
-            </div>
-            <div class="cardd col" data-aos="fade-up">
-                <p> Products Catalogue </p>
-            </div>
-            <div class="cardd col" data-aos="fade-up">
-                <p> Contact Form </p>
+                <p> Payment Gateway (Ecommerce) </p>
             </div>
             <div class="cardd col" data-aos="fade-up">
                 <p> Checkout Page (Ecommerce) </p>
             </div>
             <div class="cardd col" data-aos="fade-up">
-                <p> Content & Graphics </p>
-            </div>
-            <div class="cardd col" data-aos="fade-up">
-                <p> Social Profiles Creations </p>
-            </div>
-            <div class="cardd col" data-aos="fade-up">
-                <p> 5 Official Emails </p>
-            </div>
-            <div class="cardd col" data-aos="fade-up">
-                <p> Logo + Favicon </p>
-            </div>
-            <div class="cardd col" data-aos="fade-up">
-                <p> 24/7 Support </p>
-            </div>
-            <div class="cardd col" data-aos="fade-up">
-                <p> Analytics </p>
-            </div>
-            <div class="cardd col" data-aos="fade-up">
-                <p> Google Business Listing </p>
-            </div>
-            <div class="cardd col" data-aos="fade-up">
-                <p> Anti Hacking Tech </p>
-            </div>
-            <div class="cardd col" data-aos="fade-up">
-                <p> Unlimited Pages </p>
-            </div>
-            <div class="cardd col" data-aos="fade-up">
-                <p> XML Sitemap </p>
-            </div>
-            <div class="cardd col" data-aos="fade-up">
-                <p> Robots.txt </p>
-            </div>
-            <div class="cardd col" data-aos="fade-up">
                 <p> Order Tracking ( Ecommerce) </p>
+            </div>
+            <div class="cardd col" data-aos="fade-up">
+                <p> Blog </p>
             </div>
             <div class="cardd col" data-aos="fade-up">
                 <p> Much More... </p>
