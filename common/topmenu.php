17b17506d59141b22432d8c826d8d3f50d0d934f
Vinod Kumar <webindianlion@gmail.com>
2022-01-24 13:46:08 +0530

    remove .php extension

--- common/topmenu.php
+++ common/topmenu.php
@@ -1,7 +1,7 @@
 
 <nav class="navbar navbar-dark  topMenu nav" aria-label="Fourth navbar example">
     <div class="container">
-      <h2 class="logo"> <a href="index.php"> inWebService </a> </h2>
+      <h2 class="logo"> <a href="index"> inWebService </a> </h2>
         <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -12,10 +12,10 @@
               <a class="nav-link active" > <i class="far fa-envelope"></i> webindianlion@gamil.com </a> 
               <a class="nav-link active border-end" ><i class="fas fa-mobile-alt"></i> +91 9810272223 </a>
             </li> -->
-            <li class="nav-item pt-2"> <a class="nav-link"  href="index.php"> Home </a> </li> 
+            <li class="nav-item pt-2"> <a class="nav-link"  href="index"> Home </a> </li> 
             <li class="nav-item pt-2"> <a class="nav-link" href="#"> About </a>  </li>
             <li class="nav-item pt-2"> <a class="nav-link" href="#"> Service </a> </li>
-            <li class="nav-item pt-2"> <a class="nav-link" href="contact.php" > Contact </a> </li>          
+            <li class="nav-item pt-2"> <a class="nav-link" href="contact" > Contact </a> </li>          
           </ul>
         </div>
     </div>
